32fb005f7074e5a92094f9f20a16076fbdd53d97
deborafurieri <debora.castiglioni@meupag.com.br>
2019-11-20 18:55:36 -0300

    minor

--- plant/plant.js
+++ plant/plant.js
@@ -21,19 +21,19 @@ const index = (arr, index) => {
 }
 
 while(true) {
-    let yLenght = Y.length - 1
-    let timerLenght = timer.length - 1
-    Y.push(1.783 * index(Y, yLenght) - 0.8187 * index(Y, yLenght-1) + 0.004667 * index(X, yLenght) + 0.004374 * index(X, -2))
+    let yLength = Y.length - 1
+    let xLength = X.length - 1
+    let timerLength = timer.length - 1
 
-    if (X.length < Y.length) X.push(0)
+    Y.push(1.783 * index(Y, yLength) - 0.8187 * index(Y, yLength - 1) + 0.004667 * index(X, yLength) + 0.004374 * index(X, xLength - 1))
 
-    timer.push(timer[timerLenght] + sampleTime)
+    timer.push(timer[timerLength] + sampleTime)
 
     plot.plot(timer, Y, "color=g", "label=Sinal")
 
-    plot.ylim(Y[yLenght] - 1, Y[yLenght] + 1)
+    plot.ylim(Y[yLength] - 1, Y[yLength] + 1)
 
-    socket.emit("message", Y[yLenght])
+    socket.emit("message", Y[yLength])
 
     if (Y.length > 100) {
         Y.splice(0)
